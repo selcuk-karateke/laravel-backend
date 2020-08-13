1fc5eccd71c2471b0e1cb7051d9221e6c8541136
Selcuk Karateke <selcuk.karateke@live.de>
2020-08-13 14:02:12 +0200

    Next Big Update :D
    Faker Integrated

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,22 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UserSeeder::class);
+        DB::statement('SET FOREIGN_KEY_CHECKS=0');
+        DB::table('roles')->truncate();
+        DB::table('employees')->truncate();
+        DB::table('tasks')->truncate();
+        // Seeder
+//        $this->call(UserSeeder::class);
+//        $this->call(RoleSeeder::class);
+//        $this->call(EmployeeSeeder::class);
+//        $this->call(TaskSeeder::class);
+
+        // Factory
+        factory(App\Role::class, 3)->create();
+        factory(App\Task::class, 10)->create();
+        factory(App\Employee::class, 10)->create();
+//        factory(App\Employee::class, 10)->create()->each(function($employee){
+//            $employee->tasks()->save(factor(App\Task::class)->make());
+//        });
     }
 }
